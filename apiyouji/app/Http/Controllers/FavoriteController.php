10d7f3793d62124e093ce6095203fdb36274b835
digiponic-luke <luke@digiponic.co.id>
2019-08-28 19:17:39 +0700

    add favorite produk feature

---
+++ apiyouji/app/Http/Controllers/FavoriteController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+use DB;
+
+class FavoriteController extends Controller
+{
+    public function data($idcustomer)
+    {
+        $path_ = DB::table('cms_settings')->where('name', 'lokasi_penyimpanan')->get();
+        $path = $path_[0]->content;
+
+        $data = DB::table('tb_favorite_item as f')
+            ->join('tb_produk as pd', 'pd.id', '=', 'f.id_produk')
+            ->join('tb_general as kt', 'pd.kategori', '=', 'kt.id')
+            ->join('tb_general as jn', 'pd.jenis', '=', 'jn.id')
+            ->join('tb_general as st', 'pd.satuan_jual', '=', 'st.id')
+            ->select('pd.*', 'kt.keterangan as kategori', 'jn.keterangan as jenis', 'st.keterangan as satuan')
+//            ->whereIn('pd.jenis',[8,22])
+            ->where('f.id_customer', $idcustomer)
+            ->whereNull('f.deleted_at')
+            ->get();
+
+        $count = count($data);
+        for ($i = 0; $i < $count; $i++) {
+            if ($data[$i]->gambar == null)
+                $data[$i]->gambar = null;
+            else
+                $data[$i]->gambar = $path . "/" . $data[$i]->gambar;
+        }
+
+        return response()->json($data, 200);
+    }
+
+    public function tambah(Request $param)
+    {
+        $save = DB::table('tb_favorite_item')->insert([
+            "id_customer" => $param['id_customer'],
+            "id_produk"   => $param['id_produk'],
+            "created_at"  => date("Y-m-d H:i:s")
+        ]);
+        if ($save) {
+            return response()->json(['error' => false, 'msg' => 'Produk Disukai'], 200);
+        }
+        return response()->json(['error' => true, 'msg' => 'Something Gone Wrong', 'data' => null], 500);
+    }
+
+    public function hapus(Request $param)
+    {
+        $hapus = DB::table('tb_favorite_item')->where('id_customer', $param['id_customer'])->where('id_produk', $param['id_produk'])->update(['deleted_at' => date("Y-m-d H:i:s")]);
+        if ($hapus) {
+            return response()->json(['error' => false, 'msg' => 'Produk Batal Disukai'], 200);
+        }
+        return response()->json(['error' => true, 'msg' => 'Something Gone Wrong', 'data' => null], 500);
+    }
+}
--- apiyouji/routes/web.php
+++ apiyouji/routes/web.php
@@ -15,7 +15,65 @@ $router->get('/', function () use ($router) {
     // return $router->app->version();
     return 'REST API Digiponic Mobile Apps for Galaksi Organik v0.1';
 });
-// harus di daftarkan
+
+// daftar area
+$router->group(['prefix' => 'area'], function () use ($router) {
+    $router->post('/', 'AreaController@data');
+});
+
+//routing customer
+$router->group(['prefix' => 'customer'], function () use ($router) {
+    $router->get('/', 'CustomerController@all');
+    $router->get('/show/{email}', 'CustomerController@show');
+    $router->post('/store', 'CustomerController@store');
+    $router->post('/batch', 'CustomerController@batch');
+    $router->put('/update/{id}', 'CustomerController@update');
+    $router->delete('/delete/{id}', 'CustomerController@delete');
+});
+
+//routing customeraddress
+$router->group(['prefix' => 'customeraddress'], function () use ($router) {
+    $router->get('/', 'CustomerAddressController@all');
+    $router->get('/show/{customerId}', 'CustomerAddressController@show');
+    $router->post('/store', 'CustomerAddressController@store');
+    $router->post('/batch', 'CustomerAddressController@batch');
+    $router->put('/update/{addressId}', 'CustomerAddressController@update');
+    $router->delete('/delete/{addressId}', 'CustomerAddressController@delete');
+    $router->get('/default/{customerId}/{addressId}', 'CustomerAddressController@makeDefault');
+});
+
+//routing Generals
+$router->group(['prefix' => 'generals'], function () use ($router) {
+    $router->get('/', 'GeneralsController@all');
+    $router->get('/show/{id}', 'GeneralsController@show');
+    $router->post('/store', 'GeneralsController@store');
+    $router->put('/update/{id}', 'GeneralsController@update');
+    $router->delete('/delete/{id}', 'GeneralsController@delete');
+    $router->get('/payment', 'GeneralsController@payment');
+    $router->get('/min-shopping', 'GeneralsController@minShopping');
+    $router->get('/ongkir/{kecamatan}', 'GeneralsController@cekOngkir');
+    $router->get('/slider', 'GeneralsController@getSlider');
+});
+
+$router->group(['prefix' => 'transaksi'], function () use ($router) {
+    $router->post('/', 'TransaksiController@simpan');
+    $router->post('/data', 'TransaksiController@data');
+    $router->post('/data/detil', 'TransaksiController@detil');
+    $router->post('/terima-pesanan', 'TransaksiController@terimaPesanan');
+    $router->post('/batalkan', 'TransaksiController@batalkanPesanan');
+});
+
+$router->group(['prefix' => 'produk'], function () use ($router) {
+    $router->get('/', 'ProdukController@data');
+    $router->post('/filter', 'ProdukController@filter');
+});
+
+$router->group(['prefix' => 'favorite'], function () use ($router) {
+    $router->get('/data/{id_customer}', 'FavoriteController@data');
+    $router->post('/tambah', 'FavoriteController@tambah');
+    $router->post('/hapus', 'FavoriteController@hapus');
+});
+// ======================== Old route
 
 // routing product
 $router->group(['prefix' => 'product'], function () use ($router) {
@@ -26,68 +84,16 @@ $router->group(['prefix' => 'product'], function () use ($router) {
     $router->put('/update/{id}', 'ProductController@update');
     $router->delete('/delete/{id}', 'ProductController@delete');
     $router->get('/stockcard/{id}', 'ProductController@stockcard');
-   
-});
 
+});
 $router->group(['prefix' => 'tipe'], function () use ($router) {
     $router->get('/', 'TipeController@data');
     $router->get('/detil/{keterangan}', 'TipeController@detil');
-   
+
 });
 
-$router->group(['prefix' => 'produk'], function () use ($router) {
-    $router->get('/', 'ProdukController@data');
-    $router->post('/filter', 'ProdukController@filter');
-});
-
-$router->group(['prefix' => 'pelanggan'], function () use ($router) {
-    $router->post('/', 'PelangganController@simpan');
-    $router->post('/perbarui', 'PelangganController@perbarui');
-});
-
-$router->group(['prefix' => 'area'], function () use ($router) {
-    $router->post('/', 'AreaController@data');
-});
-
-$router->group(['prefix' => 'cekongkir'], function () use ($router) {
-    $router->get('/data/{id}', 'ApiCekOngkir@data');
-});
-
-
-$router->group(['prefix' => 'transaksi'], function () use ($router) {
-    $router->post('/', 'TransaksiController@simpan');
-    $router->post('/data', 'TransaksiController@data');
-    $router->post('/data/detil', 'TransaksiController@detil');
-});
-
-//routing customeraddress
-$router->group(['prefix'=>'customeraddress'], function()use ($router){
-    $router->get('/', 'customeraddressController@all');
-    $router->get('//show/{id}', 'customeraddressController@show');
-    $router->post('/store', 'customeraddressController@store');
-    $router->post('/batch', 'customeraddressController@batch');
-    $router->put('/update/{id}', 'customeraddressController@update');
-    $router->delete('/delete/{id}', 'customeraddressController@delete');
-});
-//routing customer
-$router->group(['prefix'=> 'customer'], function()use ($router){
-    $router->get('/', 'CustomerController@all');
-    $router->get('/show/{id}', 'CustomerController@show');
-    $router->post('/store', 'CustomerController@store');
-    $router->post('/batch', 'CustomerController@batch');
-    $router->put('/update/{id}', 'CustomerController@update');
-    $router->delete('/delete/{id}', 'CustomerController@delete');
-});
-//routing Generals
-$router->group(['prefix'=>'generals'],function()use($router){
-    $router->get('/', 'generalsController@all');
-    $router->get('/show/{id}', 'generalsController@show');
-    $router->post('/store', 'generalsController@store');
-    $router->put('/update/{id}', 'generalsController@update');
-    $router->delete('/delete/{id}', 'generalsController@delete');
-});
 //routing Pesan
-$router->group(['prefix'=>'pesan'],function()use($router){
+$router->group(['prefix' => 'pesan'], function () use ($router) {
     $router->get('/', 'PesanController@all');
     $router->get('/show/{id}', 'PesanController@show');
     $router->post('/store', 'PesanController@store');
@@ -95,7 +101,7 @@ $router->group(['prefix'=>'pesan'],function()use($router){
     $router->delete('/delete/{id}', 'PesanController@delete');
 });
 //routing ProductImages
-$router->group(['prefix'=>'productimages'], function()use($router){
+$router->group(['prefix' => 'productimages'], function () use ($router) {
     $router->get('/', 'ProductImagesController@all');
     $router->get('/show/{id}', 'ProductImagesController@show');
     $router->post('/store', 'ProductImagesController@store');
@@ -103,7 +109,7 @@ $router->group(['prefix'=>'productimages'], function()use($router){
     $router->delete('/delete/{id}', 'ProductImagesController@delete');
 });
 //routing Promotions
-$router->group(['prefix'=>'promotion'], function()use($router){
+$router->group(['prefix' => 'promotion'], function () use ($router) {
     $router->get('/', 'PromotionController@all');
     $router->get('/promotion/show/{id}', 'PromotionController@show');
     $router->post('/promotion/store', 'PromotionController@store');
@@ -112,7 +118,7 @@ $router->group(['prefix'=>'promotion'], function()use($router){
     $router->delete('/promotion/delete/{id}', 'PromotionController@delete');
 });
 //routing PromotionDetail
-$router->group(['prefix'=>'promotiondetail'], function()use($router){
+$router->group(['prefix' => 'promotiondetail'], function () use ($router) {
     $router->get('/', 'PromotionDetailController@all');
     $router->get('/show/{id}', 'PromotionDetailController@show');
     $router->post('/store', 'PromotionDetailController@store');
@@ -121,7 +127,7 @@ $router->group(['prefix'=>'promotiondetail'], function()use($router){
     $router->delete('/delete/{id}', 'PromotionDetailController@delete');
 });
 //routing SalesOrder
-$router->group(['prefix'=>'salesorders'], function()use($router){
+$router->group(['prefix' => 'salesorders'], function () use ($router) {
     $router->get('/', 'SalesOrdersController@all');
     $router->get('/show/{id}', 'SalesOrdersController@show');
     $router->post('/store', 'SalesOrdersController@store');
@@ -130,7 +136,7 @@ $router->group(['prefix'=>'salesorders'], function()use($router){
     $router->delete('/delete/{id}', 'SalesOrdersController@delete');
 });
 //routing SalesOrderDetail
-$router->group(['prefix'=>'salesorderdetail'], function()use($router){
+$router->group(['prefix' => 'salesorderdetail'], function () use ($router) {
     $router->get('/', 'SalesOrderDetailController@all');
     $router->get('/show/{id}', 'SalesOrderDetailController@show');
     $router->post('/store', 'SalesOrderDetailController@store');
@@ -139,7 +145,7 @@ $router->group(['prefix'=>'salesorderdetail'], function()use($router){
     $router->delete('/delete/{id}', 'SalesOrderDetailController@delete');
 });
 //routing stockCards
-$router->group(['prefix'=>'stockcards'], function()use($router){
+$router->group(['prefix' => 'stockcards'], function () use ($router) {
     $router->get('/', 'StockCardsController@all');
     $router->get('/show/{id}', 'StockCardsController@show');
     $router->post('/store', 'StockCardsController@store');
@@ -148,7 +154,7 @@ $router->group(['prefix'=>'stockcards'], function()use($router){
     $router->delete('/delete/{id}', 'StockCardsController@delete');
 });
 //routing Users
-$router->group(['prefix'=>'users'], function()use($router){
+$router->group(['prefix' => 'users'], function () use ($router) {
     $router->get('/', 'UsersController@all');
     $router->get('/show/{id}', 'UsersController@show');
     $router->post('//store', 'UsersController@store');
@@ -156,7 +162,7 @@ $router->group(['prefix'=>'users'], function()use($router){
     $router->delete('/delete/{id}', 'UsersController@delete');
 });
 //routing cities
-$router->group(['prefix'=>'cities'], function()use($router){
+$router->group(['prefix' => 'cities'], function () use ($router) {
     $router->get('/', 'CitiesController@all');
     $router->get('/show/{id}', 'CitiesController@show');
     $router->post('/store', 'CitiesController@store');
@@ -164,7 +170,7 @@ $router->group(['prefix'=>'cities'], function()use($router){
     $router->delete('/delete/{id}', 'CitiesController@delete');
 });
 //routing districs
-$router->group(['prefix'=>'districs'], function()use($router){
+$router->group(['prefix' => 'districs'], function () use ($router) {
     $router->get('/', 'DistricsController@all');
     $router->get('/show/{id}', 'DistricsControlle@show');
     $router->post('/store', 'DistricsController@store');
@@ -172,12 +178,12 @@ $router->group(['prefix'=>'districs'], function()use($router){
     $router->delete('/delete/{id}', 'DistricsControlle@delete');
 });
 //routing states
-$router->group(['prefix'=>'states'], function()use($router){
-$router->get('/', 'StatesController@all');
-$router->get('/show/{id}', 'StatesController@show');
-$router->post('/store', 'StatesController@store');
-$router->put('/update/{id}', 'StatesController@update');
-$router->delete('/delete/{id}', 'StatesController@delete');
+$router->group(['prefix' => 'states'], function () use ($router) {
+    $router->get('/', 'StatesController@all');
+    $router->get('/show/{id}', 'StatesController@show');
+    $router->post('/store', 'StatesController@store');
+    $router->put('/update/{id}', 'StatesController@update');
+    $router->delete('/delete/{id}', 'StatesController@delete');
 });
 //routing transaksi
 $router->get('transactions/', 'TransactionsController@all');
